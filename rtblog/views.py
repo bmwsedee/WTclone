f507c847263c89b0e48908717565209f8711153d
florian <florian.m.d@hotmail.nl>
2018-07-14 18:08:47 +0200

    entire tutorial exceot end

--- rtblog/views.py
+++ rtblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .models import timezone
 from .forms import PostForm
@@ -8,21 +8,19 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'rtblog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'rtblog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.title == "post_title"
-            post.author == request.user
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_list', pk=post.pk)
-
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'rtblog/post_edit.html', {'form': form})
-
-#def post_detail(request, pk):
-    #post = get_object_or_404(Post, pk=pk)
-    #return render(request, 'blog/post_detail.html', {'post': post})
