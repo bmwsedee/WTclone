4c9d08ee36dea10debcf07e7cf9b46087f7f2152
florian <florian.m.d@hotmail.nl>
2018-07-14 15:38:49 +0200

    l

--- rtblog/migrations/0001_initial.py
+++ rtblog/migrations/0001_initial.py
@@ -12,9 +12,9 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+    #dependencies = [
+    #    migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    #]
 
     operations = [
         migrations.CreateModel(
